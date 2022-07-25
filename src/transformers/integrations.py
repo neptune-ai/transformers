e0201dc48b1ba52cd7245f0d0bb9cd212b167c89
Rafal Jankowski <rafal.jankowski@neptune.ai>
2022-07-25 16:17:40 +0200

    Added support of hyperparameter search

--- src/transformers/integrations.py
+++ src/transformers/integrations.py
@@ -1107,6 +1107,13 @@ class NeptuneCallback(TrainerCallback):
         if state and hasattr(state, 'trial_params') and state.trial_params is not None:
             self._metadata_namespace['trial_params'] = state.trial_params
 
+    def _log_hyper_param_search_parameters(self, state):
+        if state and hasattr(state, 'trial_name'):
+            self._metadata_namespace['trial'] = state.trial_name
+
+        if state and hasattr(state, 'trial_params') and state.trial_params is not None:
+            self._metadata_namespace['trial_params'] = state.trial_params
+
     def on_train_begin(self, args, state, control, model=None, **kwargs):
         self._ensure_run_with_monitoring()
         self._force_reset_monitoring_run = True
